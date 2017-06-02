bd749eded0f50641c91a4f428892c7f42e45b878
FDD <smileFDD@gmail.com>
2017-06-02 22:53:10 +0800

    # FIX event handle # ADD DOM容器可选择

--- dist/ol3Echarts.js
+++ dist/ol3Echarts.js
@@ -92,7 +92,7 @@ var echarts = __webpack_require__(0)
  * 创建独立对象
  * @param map
  */
-var ol3Echarts = function (map) {
+var ol3Echarts = function (map, container) {
   this.map = map
   var size = this.map.getSize()
   var div = document.createElement('div')
@@ -101,7 +101,15 @@ var ol3Echarts = function (map) {
   div.style.width = size[0] + 'px'
   div.style.top = 0
   div.style.left = 0
-  this.map.getViewport().appendChild(div)
+  if (container && container.indexOf('.') === 0) {
+    var _container = this.getElementsByClassName(container, window)
+    _container.appendChild(div)
+  } else if (container && container.indexOf('#') === 0) {
+    var _con = (typeof id === 'string' ? document.getElementById(id) : id);
+    _con.appendChild(div)
+  } else {
+    this.map.getViewport().appendChild(div)
+  }
   this._echartsContainer = div
   this.chart = echarts.init(div)
   if (!echarts) {
@@ -110,6 +118,20 @@ var ol3Echarts = function (map) {
   echarts.Map = map
   this.resize()
 }
+
+/**
+ * 通过类名获取元素
+ * @param str
+ * @param root
+ * @returns {HTMLElement}
+ */
+ol3Echarts.prototype.getElementsByClassName = function (str, root) {
+  var _root = root || window
+  var $ = _root.document.querySelector.bind(_root.document)
+  var target = $(str)
+  return target
+}
+
 /**
  * 移除echarts
  */
@@ -126,8 +148,30 @@ ol3Echarts.prototype.resize = function () {
   var size = this.map.getSize()
   that._echartsContainer.style.width = size[0] + 'px'
   that._echartsContainer.style.height = size[1] + 'px'
-  window.onresize = function () {
-    that.chart.resize()
+  var resizeEvt = (('orientationchange' in window) ? 'orientationchange' : 'resize')
+  var doc = window.document;
+  window.addEventListener(resizeEvt, function() {
+    setTimeout(function () {
+      that.chart.resize()
+    }, 50)
+  }, false);
+  window.addEventListener('pageshow', function(e) {
+    if (e.persisted) {
+      setTimeout(function () {
+        that.chart.resize()
+      }, 50)
+    }
+  }, false);
+  if (doc.readyState === 'complete') {
+    setTimeout(function () {
+      that.chart.resize()
+    }, 50)
+  } else {
+    doc.addEventListener('DOMContentLoaded', function(e) {
+      setTimeout(function () {
+        that.chart.resize()
+      }, 50)
+    }, false);
   }
 }
 
--- src/layer/echartsLayer.js
+++ src/layer/echartsLayer.js
@@ -3,7 +3,7 @@ var echarts = require('echarts')
  * 创建独立对象
  * @param map
  */
-var ol3Echarts = function (map) {
+var ol3Echarts = function (map, container) {
   this.map = map
   var size = this.map.getSize()
   var div = document.createElement('div')
@@ -12,7 +12,15 @@ var ol3Echarts = function (map) {
   div.style.width = size[0] + 'px'
   div.style.top = 0
   div.style.left = 0
-  this.map.getViewport().appendChild(div)
+  if (container && container.indexOf('.') === 0) {
+    var _container = this.getElementsByClassName(container, window)
+    _container.appendChild(div)
+  } else if (container && container.indexOf('#') === 0) {
+    var _con = (typeof id === 'string' ? document.getElementById(id) : id);
+    _con.appendChild(div)
+  } else {
+    this.map.getViewport().appendChild(div)
+  }
   this._echartsContainer = div
   this.chart = echarts.init(div)
   if (!echarts) {
@@ -21,6 +29,20 @@ var ol3Echarts = function (map) {
   echarts.Map = map
   this.resize()
 }
+
+/**
+ * 通过类名获取元素
+ * @param str
+ * @param root
+ * @returns {HTMLElement}
+ */
+ol3Echarts.prototype.getElementsByClassName = function (str, root) {
+  var _root = root || window
+  var $ = _root.document.querySelector.bind(_root.document)
+  var target = $(str)
+  return target
+}
+
 /**
  * 移除echarts
  */
@@ -37,8 +59,30 @@ ol3Echarts.prototype.resize = function () {
   var size = this.map.getSize()
   that._echartsContainer.style.width = size[0] + 'px'
   that._echartsContainer.style.height = size[1] + 'px'
-  window.onresize = function () {
-    that.chart.resize()
+  var resizeEvt = (('orientationchange' in window) ? 'orientationchange' : 'resize')
+  var doc = window.document;
+  window.addEventListener(resizeEvt, function() {
+    setTimeout(function () {
+      that.chart.resize()
+    }, 50)
+  }, false);
+  window.addEventListener('pageshow', function(e) {
+    if (e.persisted) {
+      setTimeout(function () {
+        that.chart.resize()
+      }, 50)
+    }
+  }, false);
+  if (doc.readyState === 'complete') {
+    setTimeout(function () {
+      that.chart.resize()
+    }, 50)
+  } else {
+    doc.addEventListener('DOMContentLoaded', function(e) {
+      setTimeout(function () {
+        that.chart.resize()
+      }, 50)
+    }, false);
   }
 }
 
